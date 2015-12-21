cfeb14c068fab53b2df1e519617f0cfceea4c9df
fortyrunner <fortyrunner@gmail.com>
2015-12-21 06:53:22 +0000

    Create a  problem template

---
+++ src/problemn.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+    "fmt"
+    "time"
+)
+
+
+func main() {
+
+    fmt.Println("Problem n. ")
+    start := time.Now()
+
+    answer := 0
+
+    fmt.Printf("-->Answer: The answer is %d\n", answer)
+    fmt.Printf("took=%s\n\n", time.Since(start))
+
+
+}
+
+
+
